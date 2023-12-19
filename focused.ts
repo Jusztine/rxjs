250506087ca98fd64565b4f63b413e3c66dc7608
Jusztine <135501012+Jusztine@users.noreply.github.com>
2023-12-19 12:39:55 +0800

    Update focused.ts

--- focused.ts
+++ focused.ts
@@ -12,3 +12,40 @@ export type FocusedFollowUpProspect = DueToCallContact;
  export type UnworkedProspect = DueToCallContact;
  
  export type ContactVariant = DueToCallContact | UnworkedProspect;
+
+//test
+
+  {
+    path: 'focused-follow-up',
+    component: DueToCallComponent,
+    data: {
+      title: 'Focused Follow Up',
+      icon: 'adjust',
+      menuLocation: mainMenu,
+      menuGroup: dailyFollowup,
+      showDefaultToolbar: true,
+      multiFilter: true,
+      hideTitle: true,
+      filterOption: SupportedFilterOption.ComponentProvided,
+      forceSelectTags: false,
+      rememberFilters: true,
+    },
+    children: [
+      {
+        path: '',
+        runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+        canActivate: [
+          GetLayoutsActivateFn,
+          GetRecordsCanActivateFn,
+          GetContactGuardFn,
+        ],
+        data: {
+          layouts: [
+            { eventName: 'ContactFields', formName: 'ContactFieldsForm' },
+            { eventName: 'NextStepLogged', formName: 'NextStepLoggedForm' },
+          ],
+        },
+        component: ContactComponent,
+      },
+    ],
+  },
